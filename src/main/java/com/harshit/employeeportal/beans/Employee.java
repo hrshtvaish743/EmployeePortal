0b707f6478841e01449e2b0fc37d4b03baa4c1fd
Harshit Vaish <hvaish01@in.ibm.com>
2021-07-21 06:53:51 +0530

    Added Employee Entity

---
+++ src/main/java/com/harshit/employeeportal/beans/Employee.java
@@ -0,0 +1,37 @@
+package com.harshit.employeeportal.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "date_of_birth")
+    private String dateOfBirth;
+
+    @Column(name = "department")
+    private String department;
+}
