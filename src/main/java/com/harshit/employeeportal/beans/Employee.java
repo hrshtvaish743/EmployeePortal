f2a596d715752257b3be0ca43e9456a657255da5
Harshit Vaish <hvaish01@in.ibm.com>
2021-07-26 12:31:01 +0530

    Added Hibernate Validation

--- src/main/java/com/harshit/employeeportal/beans/Employee.java
+++ src/main/java/com/harshit/employeeportal/beans/Employee.java
@@ -1,29 +1,33 @@
 package com.harshit.employeeportal.beans;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
-import java.util.Date;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "employee")
+@RequiredArgsConstructor
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
 
     @Column(name = "first_name")
+    @NotEmpty
+    @NotNull
     private String firstName;
 
     @Column(name = "last_name")
+    @NotEmpty
+    @NotNull
     private String lastName;
 
     @Column(name = "gender")
@@ -34,4 +38,12 @@ public class Employee {
 
     @Column(name = "department")
     private String department;
+
+    public Employee(String firstName, String lastName, String gender, String dateOfBirth, String department) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.department = department;
+    }
 }
